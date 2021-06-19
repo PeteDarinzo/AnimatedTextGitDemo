aecc52768f9863b6b3e67b0f2f4a43f6c89bf7e3
Peter Darinzo <birel44@yahoo.com>
2021-06-19 16:42:37 -0500

    restrict color to purple

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 function randomRGB() {
     const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
+    //const g = Math.floor(Math.random() * 256);
+    g = 0;
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r},${g},${b})`;
   }
